1fd94dd16548738ca0a80f1c958c0a9fb6921aac
nabilasiregar <52696885+nabilasiregar@users.noreply.github.com>
2024-03-14 01:04:58 +0100

    improve model

--- src/models/tune_random_forests.py
+++ src/models/tune_random_forests.py
@@ -1,16 +1,22 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from random_forests_model import RandomForestModel
 
 if __name__ == "__main__":
-    data = pd.read_csv('../../data/processed/train_data.csv').dropna(subset=['msi_status'])
-    X = data.iloc[:, :-1]
-    y = data.iloc[:, -1].astype(str)
+    train_data = pd.read_csv('../../data/processed/train_data.csv')
+    X_train = train_data.iloc[:, :-1]
+    y_train = train_data.iloc[:, -1].astype(str)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    val_data = pd.read_csv('../../data/processed/val_data.csv')
+    X_val = val_data.iloc[:, :-1]
+    y_val = val_data.iloc[:, -1].astype(str)
 
     model = RandomForestModel(random_state=42)
-    best_params, best_score = model.tune_hyperparameters(X_train, y_train, X_test, y_test, n_trials=100)
 
-    model.save_model("random_forest_model.joblib")
-    print(f"Best Model: {best_params['n_estimators']} Trees, Accuracy: {best_score}")
+    # Tune hyperparameters using the validation set for evaluation
+    best_params, best_score = model.tune_hyperparameters(X_train, y_train, X_val, y_val, n_trials=100)
+
+    model.save_model("best_random_forest_model.joblib")
+
+    print(f"Best Model Parameters: {best_params}")
+    print(f"Best number of trees: {best_params['n_estimators']}")
+    print(f"Best Validation Score: {best_score}")
