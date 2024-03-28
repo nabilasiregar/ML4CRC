84a918a0f421d5c095b9df6314cac83df10dc5e3
nabilasiregar <52696885+nabilasiregar@users.noreply.github.com>
2024-03-28 01:40:24 +0100

    update oversampling

--- src/data/oversampling.py
+++ src/data/oversampling.py
@@ -23,19 +23,19 @@ def oversample_data(train_features_path, train_labels_path, output_dir):
     X_train_smote, y_train_smote = smote.fit_resample(X_train_scaled_df, y_train)
     
     # Save the oversampled data with feature names
-    pd.DataFrame(X_train_smote, columns=feature_names).to_csv(f"{output_dir}/train_features_smote.csv", index=False)
-    pd.DataFrame(y_train_smote, columns=[y_train.name]).to_csv(f"{output_dir}/train_labels_smote.csv", index=False)
+    pd.DataFrame(X_train_smote, columns=feature_names).to_csv(f"{output_dir}/train_features_80:20_smote.csv", index=False)
+    pd.DataFrame(y_train_smote, columns=[y_train.name]).to_csv(f"{output_dir}/train_labels_80:20_smote.csv", index=False)
     
     # Save the scaler model for later use
-    scaler_model_path = f"{output_dir}/scaler_model.joblib"
+    scaler_model_path = f"{output_dir}/scaler_model_80:20.joblib"
     joblib.dump(scaler, scaler_model_path)
 
     return smote
 
 # Define paths
 output_dir = '../../data/final/'
-train_features_path = f"{output_dir}/train_features.csv"
-train_labels_path = f"{output_dir}/train_labels.csv"
+train_features_path = f"{output_dir}/train_features_80:20.csv"
+train_labels_path = f"{output_dir}/train_labels_80:20.csv"
 
 # Execute the function
 smote = oversample_data(train_features_path, train_labels_path, output_dir)
